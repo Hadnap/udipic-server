c050f823f6325159233478fb8ec777c28d7354d6
Raphaël HERVEUX <herveuxr@gmail.com>
2021-04-08 19:42:39 +0200

    Add fastify-static

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,9 @@
-'use strict'
-
+"use strict";
+const path = require("path");
+console.log(path.join(__dirname, "../../udipic-app/build/index.html"));
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.register(require("fastify-static"), {
+    root: path.join(__dirname, "../../udipic-app/build"),
+    prefix: "/",
+  });
+};
